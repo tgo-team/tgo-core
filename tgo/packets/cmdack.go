94de6d961485413d01df9d1f928ce9e0d77d5960
tangtao <tt@qihyunxin.com>
2019-04-03 17:31:18 +0800

    修改命令回执包

--- tgo/packets/cmdack.go
+++ tgo/packets/cmdack.go
@@ -6,3 +6,14 @@ type CmdackPacket struct {
 	Status  uint16 // 状态
 	Payload []byte // 消息内容
 }
+
+func NewCmdackPacketWithHeader(fh FixedHeader) *CmdackPacket {
+	c := &CmdackPacket{}
+	c.FixedHeader = fh
+	return c
+}
+
+func NewCmdackPacket(cmd string,status uint16, payload []byte) *CmdackPacket {
+
+	return &CmdackPacket{CMD: cmd,Status:status, Payload: payload, FixedHeader: FixedHeader{PacketType: Cmd}}
+}
